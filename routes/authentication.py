be77081d14d7394f309f0a9dfc4e15fee9f14a52
elvus <lvssgv@gmail.com>
2025-01-27 20:40:24 -0300

    feat: Add refresh token endpoint

--- routes/authentication.py
+++ routes/authentication.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, jsonify, request
-from flask_jwt_extended import create_access_token
+from flask_jwt_extended import create_access_token, get_jwt_identity, jwt_required
+from flask_jwt_extended import create_refresh_token
 
 from connection.connection import Connection
 from models.user_model import User
@@ -21,9 +22,18 @@ def login():
         
         user = User(**cursor)
         access_token = create_access_token(identity=user.id)
-        return {'status': 'success', 'user': user.to_json(), 'access_token': access_token}, 200
+        refresh_token = create_refresh_token(identity=user.id)
+        return {'status': 'success', 'user': user.to_json(), 'access_token': access_token, 'refresh_token': refresh_token}, 200
     except Exception as e:
         return {"status": "failed", "msg": "Internal server error"}, 500
+
+@authentication.route('/v1/stream/app/refresh', methods=['POST'])
+@jwt_required(refresh=True)
+def refresh():
+    identity = get_jwt_identity()
+    access_token = create_access_token(identity=identity)
+    return {'status': 'success', 'access_token': access_token}, 200
+
 
 @authentication.route('/v1/stream/app/register', methods=['POST'])
 def register():
